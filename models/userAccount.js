968a6d3ae3b30f949d5648d4c6dbf0287be6f26d
Ana Lee <analee@D-10-156-31-37.dhcp4.washington.edu>
2019-02-07 20:11:55 -0800

    removed bugs

--- models/userAccount.js
+++ models/userAccount.js
@@ -1,20 +1,15 @@
-// var Sequelize = require("sequelize");
-// var sequelize = require("/config/config.json");
-
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define("user", {
-   apiID: {
+    apiID: {
       type: DataTypes.STRING,
       allowNull: false,
-      autoIncrement: true,
       primaryKey: true
     },
     zipcode: {
       type: DataTypes.INTEGER,
       allowNull: false,
       len: [1]
-    },
-
+    }
   });
   //commenting this out until we have more understanding of how we are routing/using the data
   // user.associate = function(models) {
